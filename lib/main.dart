9d073fec621aa979a31276c3d7b4d252b780c881
kritartha bikram shah <thisiskritartha@gmail.com>
2023-01-27 21:50:11 +0545

    Replaced the previous(Base) model with custom(tflite) image labeling mlkit library.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 
 void main() {
   runApp(const MyHomePage());
@@ -26,10 +29,14 @@ class _MyHomePageState extends State<MyHomePage> {
   void initState() {
     super.initState();
     picker = ImagePicker();
+
     //TODO: Initialize labeler
-    final ImageLabelerOptions options =
-        ImageLabelerOptions(confidenceThreshold: 0.5);
-    imageLabeler = ImageLabeler(options: options);
+    //This is for the base(default) Model of image labeling.
+    // final ImageLabelerOptions options =
+    //     ImageLabelerOptions(confidenceThreshold: 0.5);
+    // imageLabeler = ImageLabeler(options: options);
+
+    createLabeler();
   }
 
   @override
@@ -44,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
     result = '';
     for (ImageLabel label in labels) {
       final String text = label.label;
-      final int index = label.index;
+      //final int index = label.index;
       final String confidence = (label.confidence).toStringAsFixed(2);
       result += '$text   $confidence \n';
     }
@@ -72,6 +79,28 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  //This time tflite(Custom) model is used instead of default mlkit library.
+  createLabeler() async {
+    final modelPath = await getModel('assets/ml/mobilenet.tflite');
+    final options = LocalLabelerOptions(modelPath: modelPath);
+    imageLabeler = ImageLabeler(options: options);
+  }
+
+  Future<String> getModel(String assetPath) async {
+    if (Platform.isAndroid) {
+      return 'flutter_assets/$assetPath';
+    }
+    final path = '${(await getApplicationSupportDirectory()).path}/$assetPath';
+    await Directory(dirname(path)).create(recursive: true);
+    final file = File(path);
+    if (!await file.exists()) {
+      final byteData = await rootBundle.load(assetPath);
+      await file.writeAsBytes(byteData.buffer
+          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    }
+    return file.path;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -92,21 +121,10 @@ class _MyHomePageState extends State<MyHomePage> {
                   margin: const EdgeInsets.only(top: 100),
                   child: Stack(
                     children: [
-                      Stack(
-                        children: [
-                          Center(
-                            child: Image.asset(
-                              'images/frame.png',
-                              height: 510,
-                              width: 500,
-                            ),
-                          ),
-                        ],
-                      ),
                       Center(
                         child: ElevatedButton(
                           style: ElevatedButton.styleFrom(
-                            primary: Colors.transparent,
+                            backgroundColor: Colors.transparent,
                             shadowColor: Colors.transparent,
                           ),
                           onPressed: imageFromGallery,
@@ -120,10 +138,19 @@ class _MyHomePageState extends State<MyHomePage> {
                                     width: 495,
                                     fit: BoxFit.fill,
                                   )
-                                : const SizedBox(
+                                : Container(
                                     height: 330,
                                     width: 340,
-                                    child: Icon(
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                        color: Colors.white,
+                                        boxShadow: const [
+                                          BoxShadow(
+                                            blurRadius: 25,
+                                            offset: Offset(10, 10),
+                                          )
+                                        ]),
+                                    child: const Icon(
                                       Icons.camera_alt,
                                       color: Colors.black,
                                       size: 100.0,
@@ -136,13 +163,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 Container(
-                  margin: const EdgeInsets.only(top: 20),
+                  margin: const EdgeInsets.only(top: 40),
                   child: Text(
                     result,
                     textAlign: TextAlign.center,
                     style: const TextStyle(
                       fontSize: 30,
                       fontWeight: FontWeight.bold,
+                      letterSpacing: 2,
                     ),
                   ),
                 ),
